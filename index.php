374e43fbf191d089db975645d954286ac8bfe757
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-14 13:57:52 -0400

    Added delete

--- index.php
+++ index.php
@@ -8,9 +8,16 @@ if(file_exists($entryFile)){
   $entries = [];
 }
 
-if(isset($_POST["entry"])){
-  $entry = $_POST["entry"];
-  $entries[$entry["name"]] = $entry;
+if(isset($_POST["do_what"])){
+  $do_what = strtolower($_POST["do_what"]);
+  if($_POST["entry"]){
+    $entry = $_POST["entry"];
+    if($do_what === "save"){
+      $entries[$entry["name"]] = $entry;
+    }else if($do_what === "delete"){
+      unset($entries[$entry["name"]]);
+    }
+  }
   file_put_contents($entryFile, json_encode($entries));
 }
 
@@ -28,12 +35,18 @@ foreach($entries as $entry){
   echo <<<HTML
     <h2>{$entry["name"]}</h2>
     <p>{$entry["number"]}</p>
+    <form action="#" method="POST">
+      <input type="hidden" name="do_what" value="delete" />
+      <input type="hidden" name="entry[name]" value="{$entry["name"]}" />
+      <button type="submit">Delete</button>
+    </form>
 HTML;
 }
 
 ?>
     <h2>New Entry</h2>
     <form action="#" method="POST">
+      <input type="hidden" name="do_what" value="save" />
       <input type="text" name="entry[number]" placeholder="(555)867-5309" />
       <input type="text" name="entry[name]" placeholder="John D. Smith" />
       <button type="submit">Submit</button>
