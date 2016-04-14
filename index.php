0c533860735ab8f92dcd4087bbe03445ea034520
RobertAKARobin <robertgfthomas@gmail.com>
2016-04-14 12:17:50 -0400

    Added POST form

--- index.php
+++ index.php
@@ -5,10 +5,20 @@
   </head>
   <body>
     <h1>Phpone Book</h1>
+    <h2>New Entry</h2>
+    <pre>
 <?php
 
-echo("Test");
+if(isset($_POST["entry"])){
+  print_r($_POST["entry"]);
+}
 
 ?>
+    </pre>
+    <form action="#" method="POST">
+      <input type="text" name="entry[number]" placeholder="(555)867-5309" />
+      <input type="text" name="entry[name]" placeholder="John D. Smith" />
+      <button type="submit">Submit</button>
+    </form>
   </body>
 </html>
